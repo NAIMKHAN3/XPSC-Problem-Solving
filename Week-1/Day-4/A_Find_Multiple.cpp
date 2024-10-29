411e5965e2ed48fc8717ab08c93aa6485624403f
Naim Khan <naimkhan01856@gmail.com>
2024-10-30 01:58:12 +0600

    find multiple solved

---
+++ Week-1/Day-4/A_Find_Multiple.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solved(){
+    
+}
+int main()
+{
+    int x, y, z;
+
+    cin >> x >> y >> z;
+    bool flag = false;
+    for (int i = x; i <= y; i++)
+    {
+        if (i % z == 0)
+        {
+            cout << i << endl;
+            flag = true;
+            break;
+        }
+    }
+
+    if (!flag)
+    {
+        cout << "-1" << endl;
+    }
+
+    return 0;
+}
