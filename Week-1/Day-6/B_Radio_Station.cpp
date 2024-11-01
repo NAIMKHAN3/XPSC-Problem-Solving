4f63a1c83bf497dbaf75608c5a4b981102f60648
Naim Khan <naimkhan01856@gmail.com>
2024-11-01 12:27:02 +0600

    radio station problem solved

---
+++ Week-1/Day-6/B_Radio_Station.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    map<string, string> mp;
+    for (int i = 0; i < n; i++)
+    {
+        string a, b;
+        cin >> a >> b;
+        mp[b] = a;
+    }
+   
+    for(int i = 0; i < m; i++){
+        string a, b;
+        cin >> a >> b;
+        string temp =b;
+    temp.pop_back();
+        cout<< a<<" " << b << " " << "#" << mp[temp] << endl;
+    }
+    return 0;
+}
