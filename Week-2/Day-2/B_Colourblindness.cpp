00173e0f6b1f135678aa1fc59b0d23004af313ad
Naim Khan <naimkhan01856@gmail.com>
2024-11-04 11:59:12 +0600

    color blindness problem solved

---
+++ Week-2/Day-2/B_Colourblindness.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        string one, two;
+        cin >> n >> one >> two;
+        bool flag = true;
+        for (int i = 0; i < n; i++)
+        {
+            if (one[i] == two[i]){
+                continue;
+            }
+            if(one[i] == 'R' || two[i] == 'R'){
+                flag = false;
+                break;
+            }
+        }
+        if(flag){
+            cout << "YES" << endl;
+        }else{
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
