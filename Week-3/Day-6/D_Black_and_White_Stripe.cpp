8c46e84a5b15910399f3c7cce1c98a478932a461
Naim Khan <naimkhan01856@gmail.com>
2024-11-15 21:41:02 +0600

    Black and white stripe problem solved

---
+++ Week-3/Day-6/D_Black_and_White_Stripe.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, k;
+        cin >> n >> k;
+        string str;
+        cin >> str;
+        int ans = INT_MAX;
+        int i = 0;
+        int j = 0;
+        queue<char> q;
+
+        while (j < n)
+        {
+            if (str[j] == 'W')
+            {
+                // cout << str[j];
+                q.push(str[j]);
+            }
+            if (j - i + 1 == k)
+            {
+               
+                int size = q.size();
+                ans = min(ans, size);
+                if (str[i] == 'W')
+                {
+                    q.pop();
+                }
+                i++;
+            }
+            j++;
+        }
+        cout << ans << endl;
+    }
+    return 0;
+}
